bd3827ff80f55e7d48c14e416e7e7d6165aeae87
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-12-03 13:56:35 +0100

    init demurrageable token without logic

--- contracts/DemurrageableToken.sol
+++ contracts/DemurrageableToken.sol
@@ -1,9 +1,17 @@
 // contracts/DemurrageableToken.sol
-pragma solidity ^0.5.0;
+pragma solidity >=0.6.0 <0.8.0;
 
-contract DemurrageableToken {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-  constructor() public payable{
+contract DemurrageableToken is ERC20{
+  uint256 public _demurrageRate;
 
+  constructor(string memory name, string memory symbol, uint256 initialSupply, uint256 demurrageRate) public payable ERC20(name, symbol){
+    _mint(msg.sender, initialSupply);
+    _demurrageRate = demurrageRate;
   }
+
+  function demurrageRate() public view returns (uint256) {
+    return _demurrageRate;
+}
 }
